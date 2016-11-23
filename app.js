a086fed57b905f04fd787a0fe83de825ded103c4
BoykoBonev <boykobonev@outlook.com>
2016-11-23 02:42:44 +0200

    added config file and nodemon

---
+++ app.js
@@ -0,0 +1,11 @@
+/* globals require  */
+
+const config = require("./server/config");
+
+const app = require("./server/config/application");
+
+require("./server/routers")(app, data);
+
+app.listen(config.port, () => {
+    console.log(`Running at {config.port}`);
+})
