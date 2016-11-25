af8c4deb2ede7877b994bfefdd2ee748f13df280
Todor Georgiev <todorgeorgiev96@gmail.com>
2016-11-25 17:20:02 +0200

    competitions router added

---
+++ server/controllers/competitions-controller.js
@@ -0,0 +1,22 @@
+module.exports = (data) => {
+    return {
+        getByID(req, res) {
+            // TODO: get all competitions from db and load them
+        },
+        getCreatePage(req, res) {
+            // TODO: load competitions creation page
+        },
+        joinCompetition(req, res) {
+            // TODO: post query to db
+        },
+        createCompetition(req, res) {
+            // TODO 
+        },
+        loadCompetitions(req, res) {
+            // TODO
+        },
+        likes(req, res) {
+            // TODO
+        }
+    }
+}
---
+++ server/routers/competitions-router.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+module.exports = (app, data) => {
+    const controller = require('./../controllers/competitions-controller')(data);
+
+    const router = new express.Router();
+
+    router
+        .get('/', controller.loadCompetitions)
+        .get('/:id', controller.getByID)
+        .get('/create', controller.getCreatePage)
+
+        .put('/:id/like', controller.likes)
+        .put('/:id/join', controller.joinCompetition)
+
+        .post('/create', controller.createCompetition);
+
+    app.use('/competitions', router);
+}
