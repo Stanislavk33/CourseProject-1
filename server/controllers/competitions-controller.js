65ee3da82af9f5de12053811d2f9e0c42458595b
BoykoBonev <boykobonev@outlook.com>
2016-11-26 14:55:42 +0200

    create competition controller added

--- server/controllers/competitions-controller.js
+++ server/controllers/competitions-controller.js
@@ -13,17 +13,35 @@ module.exports = (data) => {
         joinCompetition(req, res) {
             // TODO: post query to db
         },
-        createCompetition(req, res) {
+        likes(req, res) {
             // TODO 
         },
         loadCompetitions(req, res) {
             data.getAllCompetitions()
                 .then(competitions => {
                     res.render("competition-list", { res: competitions });
-                })
+                });
         },
-        likes(req, res) {
-            // TODO
+        createCompetition(req, res) {
+            let body = req.body;
+            let user = req.user.username;
+            let keys = req.body.keys
+                .split(" ")
+                .filter(x => x !== "");
+
+            data.createCompetition({
+                    place: body.place,
+                    organizator: user,
+                    category: body.category,
+                    points: body.points,
+                    level: body.level,
+                    keys: keys
+                })
+                .then(competition => {
+                    res.redirect(`/competition/${competition._id}`);
+                }).catch(err => {
+                    console.log(err);
+                });
         }
-    }
-}
+    };
+};
