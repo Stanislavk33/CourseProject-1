598552f7fda59ab20d5d5f0afc7b33c2b0a369d0
Elena Zarkova <ezarkova@gmail.com>
2016-11-27 21:38:09 +0200

    Categories added to homepage

--- server/controllers/home-controller.js
+++ server/controllers/home-controller.js
@@ -4,15 +4,17 @@ module.exports = (data) => {
             Promise.all([
                 data.getLatestUpcommingCompetitions(),
                  data.getMostPopularCompetitions(),
-                 data.getTopUsers()
+                 data.getTopUsers(),
+                 data.getAllCategories()
                  ])
-                .then(([upcommingCompetition, mostPopularCompetition, topUsers]) => {
+                .then(([upcommingCompetition, mostPopularCompetition, topUsers, categories]) => {
 
                     res.render("home-page", {
                         result: {
                             upcommingCompetition,
                             mostPopularCompetition,
-                            topUsers
+                            topUsers,
+                            categories
                         }
                     })
                 });
