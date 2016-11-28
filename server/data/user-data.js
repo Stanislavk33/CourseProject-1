70625a019502854852378ce7f8ec4dd5d3b5af2e
Elena Zarkova <ezarkova@gmail.com>
2016-11-28 22:45:30 +0200

    Added user profile page and edit profile option

--- server/data/user-data.js
+++ server/data/user-data.js
@@ -1,6 +1,6 @@
 "use strict"
 
-module.exports = function(models) {
+module.exports = function (models) {
     let User = models.User;
 
     return {
@@ -18,15 +18,15 @@ module.exports = function(models) {
         getTopUsers() {
             return new Promise((resolve, reject) => {
                 User.find({})
-                .sort({"progress.totalPoints": "desc"})
-                .limit(10)
-                .exec((err, users)=>{
-                    if(err){
-                        return reject(err);
-                    }
+                    .sort({ "progress.totalPoints": "desc" })
+                    .limit(10)
+                    .exec((err, users) => {
+                        if (err) {
+                            return reject(err);
+                        }
 
-                    return resolve(users);
-                })
+                        return resolve(users);
+                    })
             });
         },
         getUserById(id) {
@@ -40,13 +40,28 @@ module.exports = function(models) {
                 });
             })
         },
-        getUserByUsername(username) {
+        getUserByUsername(username, asPersonalPage) {
             return new Promise((resolve, reject) => {
                 User.findOne({ "username": username }, (err, user) => {
                     if (err) {
                         return reject(err);
                     }
 
+                    const joinedCompetitions = [];
+                    const attendedCompetitions = [];
+
+                    user.competitions.forEach(c => {
+                        if (c.attended) {
+                            attendedCompetitions.push(c);
+                        } else if (asPersonalPage) {
+                            joinedCompetitions.push(c);
+                        }
+                    });
+
+                    user.attendedCompetitions = attendedCompetitions;
+                    if (asPersonalPage) {
+                        user.joinedCompetitions = joinedCompetitions;
+                    }
                     return resolve(user);
                 });
             });
@@ -91,18 +106,10 @@ module.exports = function(models) {
                     })
             });
         },
-        removeCompetitionFromUser(username, competitionId){
-            return new Promise((resolve, reject) =>{
-                User.findOneAndUpdate({ "username": username }, { $push: { "competitions": competition } },
-                    (err) => {
-                        if (err) {
-                            return reject(err);
-                        }
-                        return resolve(competition);
-                    })
-            });
+        removeCompetitionFromUser(username, competitionId) {
+
         },
-        updateUserInRole(userId, role){
+        updateUserInRole(userId, role) {
             return new Promise((resolve, reject) => {
                 User.findByIdAndUpdate({ "_id": userId }, { $set: { "inRole": role } },
                     (err) => {
@@ -111,6 +118,17 @@ module.exports = function(models) {
                         }
                         return resolve();
                     });
+            });
+        },
+        updateUserInformation(username, newInfo) {
+            return new Promise((resolve, reject) => {
+                User.findOneAndUpdate({ username }, newInfo,
+                    (err, user) => {
+                        if (err) {
+                            return reject(err);
+                        }
+                        return resolve(err);
+                    })
             });
         }
         //updateProgress()
--- server/routers/users-router.js
+++ server/routers/users-router.js
@@ -1,10 +1,17 @@
 const express = require('express');
 
+const multer = require('multer');
+const upload = multer({ dest: './public/imgs/user-images/' });
+
 module.exports = (app, data) => {
-    // const controller = require('./../controllers/user-controller')(data);
+    const controller = require('./../controllers/user-controller')(data);
 
-    // const router = new express.Router();
+    const router = new express.Router();
 
+    router
+        .get("/:username", controller.getProfile)
+        .get("/:username/edit", controller.getEditPage)
+        .post("/:username/edit", upload.single('avatar'), controller.editProfile);
     // router
     //     .get('/login', controller.loadLoginPage)
     //     .post('/login', controller.login)
@@ -16,5 +23,5 @@ module.exports = (app, data) => {
 
     //     .get('/profile/:id', controller.getById)
 
-    // app.use('/users', router)
+    app.use('/users', router);
 };
