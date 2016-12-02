4e2a154d442058fafc5a429e35d0df3590fdbe1b
Silviya <silweto7@abv.bg>
2016-12-02 21:51:28 +0200

    admin can add category, and fixed show category in competitions

--- server/controllers/categories-controller.js
+++ server/controllers/categories-controller.js
@@ -15,11 +15,30 @@ module.exports = (data) => {
             const link = req.params.link;
             data.getCategoryByLink(link)
                 .then(category => {
-                    return res.status(200).render('category', { result: category, user: req.user });
+                    return res.status(200).render('category', { result: { category, user: req.user } });
                 })
                 .catch(err => {
                     res.status(500).redirect('/500');
                 })
+        },
+        getCreateCategory(req, res) {
+            return res.status(200).render('create-category', { result: { user: req.user } });
+        },
+        createCategory(req, res) {
+            let body = req.body;
+
+            data.createCategory({
+                    title: body.title,
+                    description: body.description,
+                    image: req.file ? req.file.filename : null
+                })
+                .then(category => {
+                    res.redirect(`/categories/${category._id}`);
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.status(500).redirect('/500');
+                });
         }
-    }
+    };
 };
--- server/routers/categories-router.js
+++ server/routers/categories-router.js
@@ -2,13 +2,15 @@
 
 const express = require('express');
 
-module.exports = ({ app, data }) => {
+module.exports = ({ app, data, authentication, uploadUserImage }) => {
     const controller = require('./../controllers/categories-controller')(data),
         router = new express.Router();
 
     router
         .get('/', controller.loadCategories)
         .get('/:link', controller.getCategoryByTitle)
+        .get('/create', authentication.isInRole('admin'), controller.getCreateCategory)
+        .post('/create', authentication.isInRole('admin'), uploadUserImage.single('avatar'), controller.createCategory)
 
     app.use('/categories', router);
 };
