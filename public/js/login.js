71db7869b2e54c71afeca96669cbdaa1f200224d
Todor Georgiev <todorgeorgiev96@gmail.com>
2016-12-03 16:59:07 +0200

    login front end validation

--- public/js/login.js
+++ public/js/login.js
@@ -2,25 +2,34 @@
 
 var app = app || {};
 
+console.log(app);
+
 $("#btn-login").on("click", (ev) => {
+    ev.preventDefault();
+
     let username = $("#tb-username").val();
     let password = $("#tb-password").val();
+
 
     let data = {
         username,
         password
     }
 
+    console.log(data);
+
     app.requester.post("/auth/login", data)
         .then(resp => {
             if (resp.success) {
-                app.notificator.showNotification(resp.success, "success")
-                setTimeout(() => { window.location.href = "/" }, 1000)
+                app.notifier.showNotification(resp.success, "success");
+                setTimeout(() => { window.location.href = "/" }, 500);
             } else if (resp.error) {
-                app.notificator.showNotification(resp.error, "error")
+                app.notifier.showNotification(resp.error, "error");
+                return;
             }
         })
         .catch(err => {
+            console.log('in catch');
             console.log(err)
         });
 })
