c0c906b82f686221335234c2e977f86147ffe7ab
Silviya <silweto7@abv.bg>
2016-12-04 20:03:07 +0200

    add minified js files

---
+++ tests/controllers/user-controller-tests.js
@@ -0,0 +1,13 @@
+/* globals require describe it beforeEach afterEach*/
+
+const expect = require("chai").expect;
+const sinonModule = require("sinon");
+
+const userController = require('./../../server/controllers/user-controller');
+
+
+describe('getProfile() tests', () => {
+    const commonMock = {
+        
+    }
+});
